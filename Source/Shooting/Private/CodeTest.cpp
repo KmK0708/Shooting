dcde835fafe13a64342f7a91201e82ac5d751e9b
KmK0708 <121008851+KmK0708@users.noreply.github.com>
2022-12-19 16:18:38 +0900

    Initial commit

---
+++ Source/Shooting/Private/CodeTest.cpp
@@ -0,0 +1,199 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodeTest.h"
+
+// 기본 값을 설정 할 수 있다.
+// 생성자 함수
+ACodeTest::ACodeTest()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACodeTest::BeginPlay()
+{
+	Super::BeginPlay();
+
+#pragma region 사칙연산
+	////Add 함수에 입력값 8, 4를 넣어서 결과를 result 변수에 받는다.
+	//int32 result1 = Add(8, 4, 10);
+	//int32 result2 = Subtract(8, 4);
+	//int32 result3 = Multiply(8, 4);
+	//float result4 = Divide(3, 11);
+
+
+	//// result 변수에 있는 값을 화면에 출력한다.
+	//UE_LOG(LogTemp, Warning, TEXT("Result1 : %d"), result1);
+	//UE_LOG(LogTemp, Warning, TEXT("Result2 : %d"), result2);
+	//UE_LOG(LogTemp, Warning, TEXT("Result3 : %d"), result3);
+	//UE_LOG(LogTemp, Warning, TEXT("Result4 : %f"), result4);
+	//
+
+	////number 변수에 3을 넣는다
+	////number = 3;
+	////number2 = number;
+	////number2 = -7;
+
+	//fnumber1 = 3.14f;
+
+	//number의 값을 output 콘솔창에 출력한다.
+	/*UE_LOG(LogTemp, Warning, TEXT("number1 is %d,number2 is %d"), number, number2);
+	UE_LOG(LogTemp, Warning, TEXT("float number is %.2f"), fnumber1);
+	UE_LOG(LogTemp, Warning, TEXT("Hello World! %s"), *name2);
+
+	isTeacher = true;
+
+	if (isTeacher == true)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("그는 선생이다!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("그는 학생이다!"));
+	}*/
+
+#pragma endregion
+
+#pragma region 조건문
+	/*
+	//만일 나이가 30살 이상이면 "꼰"이라고 출력하고 싶다.
+	if (age >= 30)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("틀"));
+	}
+	//그렇지 않고, 만일 나이가 20살 이상이면 "아조씨"라고 출력하고 싶다.
+	else if (age >= 20)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("아조씨"));
+	}
+	// 그렇지 않다면, "응애"라고 출력하고 싶다.
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("응애"));
+	}
+
+	//만일 나이가 20살 이상 이면서, 키가 160 이상인 경우에는
+	if (age >= 20 && height >= 165)
+	{
+		// "환영" 이라고 출력한다
+		UE_LOG(LogTemp, Warning, TEXT("어서와요"));
+	}
+	//그렇지 않으면
+	else
+	{
+		//"넌 나가라"라고 출력한다
+		UE_LOG(LogTemp, Warning, TEXT("넌 나가라"));
+	}
+	*/
+
+
+
+
+	//만일 국적이 "한국"이면서 나이가 20살 이상 40살 미만이면
+	if (nation == TEXT("한국") && age >= 20 && age < 40)
+	{
+		//군대가라 라고 출력
+		UE_LOG(LogTemp, Warning, TEXT("군대가라"));
+
+	}
+	else
+	{
+		//아닐시 군대아님 출력
+
+		UE_LOG(LogTemp, Warning, TEXT("군대아님"));
+	}
+
+
+#pragma endregion
+
+	// 임시함수 fullName 선언, 문자열에 경우에는 *선언
+	// 한글로 출력하려면 TEXT로 감싸야함 영어는 그냥써도 상관없음
+	FString fullName = StringApender(TEXT("김"),(TEXT("주영")));
+	UE_LOG(LogTemp, Warning, TEXT("나의 이름은 : %s"), *fullName);
+	
+	int32 checker = 37;
+
+
+	// checker변수의 값이 짝수라면
+	if (checker % 2 == 0)
+	{
+		//"even"이라고 출력한다.
+		UE_LOG(LogTemp, Warning, TEXT("even"));
+	}
+	//그렇지 않고, 변수의 값이 홀수라면,
+	else
+	{
+		//"odd"라고 출력한다.
+		UE_LOG(LogTemp, Warning, TEXT("odd"));
+	}
+
+	//반복문
+	// "안녕하세요" 라는 문자열을 10번 출력한다
+
+	for (int32 i = 0; i < 10; i = i + 1)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%d"), i + 1);
+	}
+
+	//int32 numb = 3;
+	//// 누적 시킬때는 등호 앞에다 + 를 함 numb += 2;
+	//numb += 2;
+	//numb = numb + 2;
+	//// 뺼때는 등호 앞에다 - 를 함 numb -= 2;
+	//numb -= 2;
+	//numb = numb - 2;
+
+
+	//1단부터 9단까지 한번에 출력하게 하려면?
+	for (int32 i = 2; i < 10; i = i + 1)
+	{
+		for (int32 j = 1; j < 10; j = j + 1)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%d * %d = %d"), i, j, i * j);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("=============="));
+	}
+	//내부 반복문이 한싸이클 돌면 외부 반복문이 한번 증가하게 하기
+
+	/*for (int32 i = 1; i < 10; i = i + 1)
+	{
+		int32 resultM = 7 * i;
+		UE_LOG(LogTemp, Warning, TEXT("7 * %d = %d"), i, resultM);
+	}*/
+
+
+}
+// Called every frame
+void ACodeTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+int32 ACodeTest::Add(int32 num1, int32 num2, int32 num3)
+{
+	// 구현 내용적는곳 반환시 앞에 return 달기
+	return num1 + num2 + num3;
+}
+int32 ACodeTest::Subtract(int32 num1, int32 num2)
+{
+	return num1 - num2;
+}
+int32 ACodeTest::Multiply(int32 num1, int32 num2)
+{
+	return num1 * num2;
+}
+float ACodeTest::Divide(int32 num1, int32 num2)
+{
+	return (float)num1 / (float)num2;
+}
+
+// 함수이름앞에 클래스 붙여주기,클래스에 소속된 함수라는걸 알려주기위해 클래스뒤에::붙이기
+FString ACodeTest::StringApender(FString firstname1, FString lastname1)
+{
+	// 문자열은 더하기만 된다.
+	return firstname1 + lastname1;
+}
---
+++ Source/Shooting/Public/CodeTest.h
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CodeTest.generated.h"
+
+UCLASS()
+class SHOOTING_API ACodeTest : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACodeTest();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// 주석다는걸 생활화하기
+	// 순서를 정하기,명확하게 설명하기
+	// 정수형 변수 number를 설명한다
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = CodeVariable)
+	int32 number;
+	//int32 number; = 0
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=CodeVariable)
+	int32 number2;
+
+	// 실수형 변수 fnumber를 선언한다.
+	float fnumber1 = 3.14f;
+	float fnumber2 = 10;
+
+	// 문자열 변수 name을 선언한다.
+	FString name;
+	//FString name = "";
+	FString name2 = FString(TEXT("김주영"));
+
+	// 논리형 변수 isStudent를 선언한다.
+	bool isStudent;
+	//bool isStudent = false; 아무것도 안써있으면 0이나 false
+	bool isTeacher = true; 
+
+	// 문자열 2개를 받아서 문자열 1개로 합쳐서 반환하는 함수를 만들려고 한다.
+	// 함수선언 한것을 블루프린트에 불러오려면 UFUNCTION(BlueprintCallable)을 붙여준다.
+	// 종속적으로 쓰이는 함수 블루프린트로 만들려면? UFUNCTION(BlueprintPure)를 붙여준다.
+	UFUNCTION(BlueprintCallable)
+	FString StringApender(FString firstname1, FString lastname1);
+
+	int32 Add(int32 num1, int32 num2, int32 num3);
+	int32 Subtract(int32 num1, int32 num2);
+	int32 Multiply(int32 num1, int32 num2);
+	float Divide(int32 num1, int32 num2);
+
+	//UPROPERTY 는 언리얼 프로퍼티
+	UPROPERTY(EditAnywhere, Category=CodeVariable)
+	int32 age = 23;
+
+	UPROPERTY(EditAnywhere, Category = CodeVariable)
+    int32 height = 160;
+
+	UPROPERTY(EditAnywhere, Category = CodeVariable)
+	FString nation = FString(TEXT("한국"));
+
+private:
+
+
+};
